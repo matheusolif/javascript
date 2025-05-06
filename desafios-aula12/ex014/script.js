6e1189b76b29401464a36cdb96b38f8e276dd1b6
Matheus Oliveira <matheusogeral@gmail.com>
2025-05-06 20:52:54 -0300

    conclui exercicio

--- desafios-aula12/ex014/script.js
+++ desafios-aula12/ex014/script.js
@@ -8,10 +8,16 @@ function carregar () {
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
+        document.body.style.background = '#b9a26e'
+        
     }else if (hora >= 12 && hora < 18) {
         img.src = 'fototarde.png'
+        document.body.style.background = '#ad743c'
     } else {
+        document.getElementById('saudacao').innerHTML = `<h3> Boa noite</h3>`
         img.src = 'fotonoite.png'
+        document.body.style.background = '#0d181E'
+        
     }
 }
 
