f255738ea73986099b2936b6743540ddac82694a
Matheus Oliveira <matheusogeral@gmail.com>
2025-05-10 23:59:11 -0300

    criei ex017

---
+++ desafios-aula14/ex017/ex017.js
@@ -0,0 +1,13 @@
+function tabuada() {
+    let txtnumero = document.getElementById('idnumero')
+    let select = document.getElementsByClassName('idselect')
+
+    let button = document.getElementsByTagName('button')
+
+    let entrada = Number(txtnumero.value)
+    select.innerHTML= ""
+
+    for (let contador = 1; contador<=10; contador++){
+       
+    }
+}
